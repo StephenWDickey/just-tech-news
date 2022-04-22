1dd2554b9b6277a7bea55f0cf7baa7ef874ef0b1
stevey d <stphndcky22@gmail.com>
2022-04-22 16:55:35 -0700

    added model method upvote() to Post model, refactored post-route

--- models/Post.js
+++ models/Post.js
@@ -13,7 +13,37 @@ const sequelize = require('../config/connection');
 
 
 // create our Post model
-class Post extends Model {}
+class Post extends Model {
+
+    // static is a javascript keyword
+    // we are creating a 'model method' called upvote()
+    // parameters are body and models (which will be req.body)
+    // models will be an object of the models
+    static upvote(body, models) {
+      return models.Vote.create({
+        user_id: body.user_id,
+        post_id: body.post_id
+      }).then(() => {
+        // find post where vote object was created
+        return Post.findOne({
+          where: {
+            id: body.post_id
+          },
+          attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [
+              // .literal sequelize method allows us to use SQL query
+              sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+              'vote_count'
+            ]
+          ]
+        });
+      });
+    }
+  }
 
 
 
